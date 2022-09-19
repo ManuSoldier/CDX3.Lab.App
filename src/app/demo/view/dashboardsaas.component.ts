ffb6b2fa97e91ebc9231836120dbab3af1483089
gucal <gucalaliriza@outlook.com>
2022-09-19 15:56:34 +0300

    create dashboard component

---
+++ src/app/demo/view/dashboardsaas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardsaas',
+  templateUrl: './dashboardsaas.component.html',
+})
+export class DashboardSaasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
