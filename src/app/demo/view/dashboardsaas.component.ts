a5011d8f20f3bafcd4c7b04e4820e01f7394edac
gucal <gucalaliriza@outlook.com>
2022-09-20 17:08:17 +0300

    complete daily task section

--- src/app/demo/view/dashboardsaas.component.ts
+++ src/app/demo/view/dashboardsaas.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { AppBreadcrumbService } from "src/app/app.breadcrumb.service";
 
+interface DailyTask {
+    id: number;
+    checked: boolean;
+    label: string;
+    images: string[];
+}
+
 @Component({
     templateUrl: "./dashboardsaas.component.html",
 })
@@ -10,4 +17,39 @@ export class DashboardSaasComponent implements OnInit {
     }
 
     ngOnInit(): void {}
+
+    completeTask: number = 1;
+
+    dailyTasks: DailyTask[] = [
+        {
+            id: 1,
+            checked: true,
+            label: "Prepare personas",
+            images: ["sd", "asdas"],
+        },
+        {
+            id: 2,
+            checked: false,
+            label: "Prepare a user journey map",
+            images: ["sd", "asdas"],
+        },
+        {
+            id: 3,
+            checked: false,
+            label: "Prepare wireframes for onboarding screen",
+            images: ["sd", "asdas"],
+        },
+        {
+            id: 4,
+            checked: false,
+            label: "Review benchmarks",
+            images: ["sd", "asdas"],
+        },
+        {
+            id: 3,
+            checked: false,
+            label: "Let a plan with UI Team",
+            images: ["sd", "asdas"],
+        },
+    ];
 }
