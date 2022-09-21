521a55cfbf127870a3d48a015d12a8b5cc6a6102
gucal <gucalaliriza@outlook.com>
2022-09-21 13:01:56 +0300

    chart section

--- src/app/demo/view/dashboardsaas.component.ts
+++ src/app/demo/view/dashboardsaas.component.ts
@@ -52,4 +52,56 @@ export class DashboardSaasComponent implements OnInit {
             images: ["sd", "asdas"],
         },
     ];
+
+    basicData: any = {
+        labels: ["January", "February", "March", "April", "May"],
+        datasets: [
+            {
+                label: "Previous Month",
+                data: [22, 36, 11, 33, 2],
+                fill: false,
+                borderColor: "#E0E0E0",
+                tension: 0.5,
+            },
+            {
+                label: "Current Month",
+                data: [22, 16, 31, 11, 38],
+                fill: false,
+                borderColor: "#6366F1",
+                tension: 0.5,
+            },
+        ],
+    };
+    basicOptions = {
+        plugins: {
+            legend: {
+                labels: {
+                    color: "#495057",
+                    boxWidth: 12,
+                    boxHeight: 4,
+                },
+                position: "bottom",
+            },
+        },
+        elements: { point: { radius: 0 } },
+        scales: {
+            x: {
+                ticks: {
+                    color: "#495057",
+                },
+                grid: {
+                    color: "#ebedef",
+                },
+            },
+            y: {
+                ticks: {
+                    color: "#495057",
+                    stepSize: 10,
+                },
+                grid: {
+                    color: "#ebedef",
+                },
+            },
+        },
+    };
 }
